62e471330121459a9bbdc25a61d4de892741eb6e
Ethiel Blandón <ethblacub@alum.us.es>
2026-04-17 17:18:30 +0000

    models

---
+++ Video-sources/aiss/peertubeminer/models/peertube/Thumbnail.java
@@ -0,0 +1,96 @@
+
+package aiss.peertubeminer.models.peertube;
+
+import javax.annotation.processing.Generated;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "height",
+    "width",
+    "aspectRatio",
+    "fileUrl"
+})
+@Generated("jsonschema2pojo")
+public class Thumbnail {
+
+    @JsonProperty("height")
+    private Integer height;
+    @JsonProperty("width")
+    private Integer width;
+    @JsonProperty("aspectRatio")
+    private String aspectRatio;
+    @JsonProperty("fileUrl")
+    private String fileUrl;
+
+    @JsonProperty("height")
+    public Integer getHeight() {
+        return height;
+    }
+
+    @JsonProperty("height")
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    @JsonProperty("width")
+    public Integer getWidth() {
+        return width;
+    }
+
+    @JsonProperty("width")
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    @JsonProperty("aspectRatio")
+    public String getAspectRatio() {
+        return aspectRatio;
+    }
+
+    @JsonProperty("aspectRatio")
+    public void setAspectRatio(String aspectRatio) {
+        this.aspectRatio = aspectRatio;
+    }
+
+    @JsonProperty("fileUrl")
+    public String getFileUrl() {
+        return fileUrl;
+    }
+
+    @JsonProperty("fileUrl")
+    public void setFileUrl(String fileUrl) {
+        this.fileUrl = fileUrl;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Thumbnail.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("height");
+        sb.append('=');
+        sb.append(((this.height == null)?"<null>":this.height));
+        sb.append(',');
+        sb.append("width");
+        sb.append('=');
+        sb.append(((this.width == null)?"<null>":this.width));
+        sb.append(',');
+        sb.append("aspectRatio");
+        sb.append('=');
+        sb.append(((this.aspectRatio == null)?"<null>":this.aspectRatio));
+        sb.append(',');
+        sb.append("fileUrl");
+        sb.append('=');
+        sb.append(((this.fileUrl == null)?"<null>":this.fileUrl));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
---
+++ VideoSearch-sources/aiss/peertubeminer/models/peertube/Datum.java
@@ -0,0 +1,130 @@
+
+package aiss.peertubeminer.models.peertube;
+
+import javax.annotation.processing.Generated;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "id",
+    "name",
+    "description",
+    "publishedAt",
+    "language",
+    "account"
+})
+@Generated("jsonschema2pojo")
+public class Datum {
+
+    @JsonProperty("id")
+    private Integer id;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("description")
+    private String description;
+    @JsonProperty("publishedAt")
+    private String publishedAt;
+    @JsonProperty("language")
+    private Language language;
+    @JsonProperty("account")
+    private Account account;
+
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("description")
+    public String getDescription() {
+        return description;
+    }
+
+    @JsonProperty("description")
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @JsonProperty("publishedAt")
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    @JsonProperty("publishedAt")
+    public void setPublishedAt(String publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    @JsonProperty("language")
+    public Language getLanguage() {
+        return language;
+    }
+
+    @JsonProperty("language")
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    @JsonProperty("account")
+    public Account getAccount() {
+        return account;
+    }
+
+    @JsonProperty("account")
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Datum.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("id");
+        sb.append('=');
+        sb.append(((this.id == null)?"<null>":this.id));
+        sb.append(',');
+        sb.append("name");
+        sb.append('=');
+        sb.append(((this.name == null)?"<null>":this.name));
+        sb.append(',');
+        sb.append("description");
+        sb.append('=');
+        sb.append(((this.description == null)?"<null>":this.description));
+        sb.append(',');
+        sb.append("publishedAt");
+        sb.append('=');
+        sb.append(((this.publishedAt == null)?"<null>":this.publishedAt));
+        sb.append(',');
+        sb.append("language");
+        sb.append('=');
+        sb.append(((this.language == null)?"<null>":this.language));
+        sb.append(',');
+        sb.append("account");
+        sb.append('=');
+        sb.append(((this.account == null)?"<null>":this.account));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
